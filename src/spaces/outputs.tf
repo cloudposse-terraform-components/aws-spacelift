955418971603fde4ba90ef4a951d46236bcefeec
Igor Rodionov <goruha@gmail.com>
2024-11-08 13:16:00 +0100

    Initial commit

--- src/spaces/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "spaces" {
-  description = "The spaces created by this component"
-  value       = local.enabled ? local.spaces : {}
-}
-
-output "policies" {
-  description = "The policies created by this component"
-  value       = local.enabled ? local.policies : {}
-}
